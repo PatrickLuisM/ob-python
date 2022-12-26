caa11d074b5ccf29ab77530a3c5ff9dca3c45040
PatrickLuisM <patricklmo32@gmail.com>
2022-12-25 21:51:28 -0500

    Tarea 8-1

---
+++ Tarea8/tarea-8-1.py
@@ -0,0 +1,14 @@
+
+import operaciones
+
+
+def main():
+    print(operaciones.sumar(10,5))
+    print(operaciones.restar(10,5))
+    print(operaciones.multiplicar(10,5))
+    print(operaciones.dividir(10,5))
+    
+
+
+if __name__ == '__main__':
+    main()
